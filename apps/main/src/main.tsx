32885e6e17f48c1e83bb430135f8ee4d4791948e
Takahiro Matsuda <takakobem@gmail.com>
2021-03-11 18:06:57 +0900

    add code which causes babel error

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+
+class Hoge {
+  hoge = async() => {  // error!
+    console.log("hoge")
+  }
+}
+
+const hoge = new Hoge()
+hoge.hoge()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
